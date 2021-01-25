64b8cc4aa511cf5904a0aee25b11a98b955dbf5d
Vladimir Ćirković <wladimir.cirkovic@gmail.com>
2021-01-25 14:40:55 +0100

    can construct

--- src/main/java/freecodecamp/dynamic_programming/HowSum.java
+++ src/main/java/freecodecamp/dynamic_programming/HowSum.java
@@ -1,7 +1,52 @@
 package freecodecamp.dynamic_programming;
 
-public class HowSum {
-    public boolean canSum(final int targetSum, final int[] numbers) {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class HowSum {
+    // O(n^m * m) time, O(m) space
+    public List<Integer> howSum(final int targetSum, final int[] numbers) {
+        if (targetSum == 0) return new ArrayList<>();
+        if (targetSum < 0) return null;
+
+        for (final int number : numbers) {
+            final int remainder = targetSum - number;
+            final List<Integer> result = howSum(remainder, numbers);
+            if (result != null) {
+                result.add(number);
+                return result;
+            }
+        }
+
+        return null;
     }
+
+    private final Map<Integer, List<Integer>> memo = new HashMap<>();
+
+    // O(n * m^2) time, O(m^2) space
+    public List<Integer> howSumMemoized(final int targetSum, final int[] numbers) {
+        if (memo.containsKey(targetSum)) return memo.get(targetSum);
+
+        if (targetSum == 0) return new ArrayList<>();
+        if (targetSum < 0) return null;
+
+        for (final int number : numbers) {
+            final int remainder = targetSum - number;
+            final List<Integer> result = howSum(remainder, numbers);
+            if (result != null) {
+                result.add(number);
+                memo.put(targetSum, result);
+                return result;
+            }
+        }
+
+        memo.put(targetSum, null);
+        return null;
+    }
+
+    /**
+     * Best sum is variation where we keep track of shortest result
+     */
 }
--- src/test/java/freecodecamp/dynamic_programming/HowSumTest.java
+++ src/test/java/freecodecamp/dynamic_programming/HowSumTest.java
@@ -2,5 +2,20 @@ package freecodecamp.dynamic_programming;
 
 import junit.framework.TestCase;
 
+import static java.util.Arrays.asList;
+
 public class HowSumTest extends TestCase {
+    public void testHowSum() {
+        assertEquals(
+                asList(3, 2, 2),
+                new HowSum().howSum(7, new int[]{2, 3})
+        );
+    }
+
+    public void testHowSumMemoized() {
+        assertEquals(
+                asList(2, 2, 2, 2),
+                new HowSum().howSumMemoized(8, new int[]{2, 3, 5})
+        );
+    }
 }
