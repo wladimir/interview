17db7a53cba59824c0c59fa07673704099806212
Vladimir Ćirković <wladimir.cirkovic@gmail.com>
2024-07-03 12:33:48 +0300

    refactor

--- src/main/java/freecodecamp/dynamic_programming/memoization/CanConstruct.java
+++ src/main/java/freecodecamp/dynamic_programming/memoization/CanConstruct.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class CanConstruct {
     // O(n^m*n) time, O(m^2) space
     public boolean canConstruct(final String target, final List<String> wordBank) {
-        if (target.length() == 0) return true;
+        if (target.isEmpty()) return true;
 
         for (final String word : wordBank) {
             if (target.startsWith(word)) {
